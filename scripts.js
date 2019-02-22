97ef9b4a137bb4457aae603e211de51f335d5651
Ryan Cahela <ryancahela@gmail.com>
2019-02-21 21:41:14 -0800

    made everything Ireie

--- scripts.js
+++ scripts.js
@@ -1,27 +1,78 @@
-function formatQuery() {
+function formatQuery(searchTerm) {
     const baseEndpoint = "https://api.github.com";
-    return baseEndpoint;
+    const query = `/users/sdfasdfa/repos`
+
+    return baseEndpoint + query;
 
 }
+
+///users/:username/repos
 
 function getRepos(url) {
     fetch(url)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(response => {
+        if(response.ok){
+           return response.json()
+        }
+        throw new Error('something went terribly wrong!');
+    })
+    .then(responseJson => {
+        if(responseJson.length === 0) {
+        renderResults('please enter a valid user name.');
+        throw new Error('something went terribly wrong!');
+        }
+        renderResults(responseJson)
+    })
+    .catch(error => console.log(error.message));
 
 }
 
-function formHandler(event) {
-    event.preventDefault();
-    let userInput = $("[name='user-search']").val();
-    console.log(userInput);
-    getRepos(formatQuery());
+function getSearchValue() {
+    let $formEl = $("[name='user-search']");
+    let search = $formEl.val();
+    console.log($formEl);
+    $formEl.val('');
+    return search;
 }
 
 
-function main() {
-    $('form').on('submit', formHandler);
-
+function formHandler() {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        let userInput = getSearchValue();
+        console.log(userInput);
+        getRepos(formatQuery(userInput));
+    });
 }
 
-$(main());
+function createLink(innerText,url) {
+    return `<a href="${url}" target="_blank">${innerText}</a>`
+}
+
+
+function renderResults(responseJson) {
+    console.log(responseJson);
+    let $results = $('.results');
+
+        if(typeof responseJson === 'string') {
+            $results.append(`<h1 style="color:red">${responseJson}</h1>`)
+            $results.removeClass('hidden');
+            return;
+        }
+
+    let linksArray = responseJson.map(function getNameAndUrl(item) {
+        let $results = $('.results');
+        let url = item.html_url;
+        let name = item.name;
+        console.log(responseJson);
+
+        $results.append(createLink(name,url));
+        $results.removeClass('hidden');
+
+    });
+}
+
+//responseJson.html_url
+//responseJson.name
+
+$(formHandler());
